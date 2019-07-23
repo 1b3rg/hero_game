6def0adad5b2bd659c1f0fbbf409174424da2517
1b3rg <nathanvandenberg13@gmail.com>
2019-07-23 23:54:16 +1000

    Code style changes only

--- main.py
+++ main.py
@@ -5,13 +5,16 @@
 # ================
 
 # roomId is a list?
-# e.g. roomId N = 1, S = 0, E = 3, Desc = "The room was empty ..", Monster = Monster etc
-# the number following the compass direction e.g. N, S, E, W is the room that the exit in that compass direction leads
+# e.g. roomId N = 1, S = 0, E = 3, Desc = "The room was empty ..", Monster
+#  = Monster etc
+# the number following the compass direction e.g. N, S, E, W is the room that
+#  the exit in that compass direction leads
 # e.g. head north leads to room 1, roomId is set to 1
 
 # hLife (Hero Life) = 10
 # hArm (Hero Armour) = 2
-# armour is a number from 0 to 10, 0 being the easiest to hit and 10 being the hardest
+# armour is a number from 0 to 10, 0 being the easiest to hit and 10 being the
+#  hardest
 
 
 # start
@@ -21,11 +24,13 @@
 # ====
 # draw/describe the room
 # look up room attributes based on roomId
-# if the room contains a monster then event = Combat else the event is set to Other
+# if the room contains a monster then event = Combat else the event is set to
+#  Other
 
 # event
 # =====
-# each event has its own set of actions, effects, results, end of event, end of game
+# each event has its own set of actions, effects, results, end of event, end of
+#  game
 # Combat : Attack or Run Away
 # Forced Move : Move (N,S,E,W) only
 # Other : Move (later : Look, Rest e.g. rest could heal 1 point of life)
@@ -44,14 +49,16 @@
 # Action
 # ------
 # If heroTurn is TRUE then check if the player wants to Attack or Run_Away
-# If heroTurn is FALSE i.e. its the Monster's turn, then always set to Action to Attack
+# If heroTurn is FALSE i.e. its the Monster's turn, then always set to Action
+# to Attack
 # If Run_Away then End_Combat_Event
 # Else if Attack then go to Effect
 
 # Effect
 # ------
 # Check hit or miss
-# to check hit, the attacker roles a 1d10 (add modifiers later e.g. strength, potions, rings etc)
+# to check hit, the attacker roles a 1d10 (add modifiers later e.g. strength,
+#  potions, rings etc)
 # if the attackRoll > armour then hit else miss
 
 # If hit then calculate damage, dmg = 1d10 x (1+Weapon)
@@ -82,6 +89,7 @@
 
 # Forced_Move
 # ===========
-# A forced move is where the Hero is running away from a monster, so must choose a direction to run to
+# A forced move is where the Hero is running away from a monster, so must
+# choose a direction to run to
 
 # add a line to test upload to Github
